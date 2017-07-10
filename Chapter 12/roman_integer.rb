3ecd27ed23a8f8ff32fa8dbc2fd0167b951cb2d6
Owen Turkle <owen@turkle.net>
2017-07-10 23:35:11 +0700

    finished roman_integer

--- Chapter 12/roman_integer.rb
+++ Chapter 12/roman_integer.rb
@@ -1,12 +1,8 @@
 def numeral_to_integer
     puts "Give us a year using roman numerals (M, D, C, V, X, or I) and I'll convert it to an integer!"
     roman = gets.chomp.downcase
-
-    if roman == ""
-        numeral_to_integer
-    end
-
     roman_array = roman.split("")
+    index = roman_array.length - 1
 
     numerals = { 'i' => 1,
                  'v' => 5,
@@ -19,16 +15,21 @@ def numeral_to_integer
     }
 
     final_integer = 0
-    roman_length = roman_array.length - 1
 
-    while roman_length >= 0
-      if numerals[roman_array[roman_length]] != nil 
-        final_integer += numerals[roman_array[roman_length]]
-        roman_length -= 1
-      else 
-        puts "Invalid letters! Try again."
-        numeral_to_integer
-      end
+    while true
+        if numerals[roman_array[index]] == nil || roman == ""
+            puts "Invalid letters! Try again."
+            roman = gets.chomp.downcase
+            roman_array = roman.split("")
+            index = roman_array.length - 1
+            final_integer = 0
+        else
+            final_integer += numerals[roman_array[index]]
+            index -= 1
+            if index < 0
+                break
+            end
+        end 
     end
 
     puts final_integer
