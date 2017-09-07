d3b6306b4ac0db0fe9eb74169e56b6b336fee6b3
Owen Turkle <owen@turkle.net>
2017-09-07 12:43:52 -0700

    updated  in global logger

--- Chapter 14/global_logger.rb
+++ Chapter 14/global_logger.rb
@@ -2,20 +2,18 @@ $nesting_depth = ''
 
 def log(description, &block)
   puts "#{$nesting_depth}#{description} started"
+  $nesting_depth << ' '
   result = block.call
+  $nesting_depth.sub!(' ', '')
   puts "#{$nesting_depth}#{description} ended; block output: #{result}"
 end
 
 log('First log') do 
-  $nesting_depth << ' '
   log('Second log') do
-    $nesting_depth << ' '
     log('Third log') do
       'confirmed'
     end
-    $nesting_depth.sub!(' ', '')
     'confirmed'
   end
-  $nesting_depth = ''
   'confirmed'
 end
